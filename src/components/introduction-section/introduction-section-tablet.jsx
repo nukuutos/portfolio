ca6cc7fc0422ad46ae9ed92891d78c100435536e
nukuutos <nukuutos@gmail.com>
2022-08-02 16:16:29 +1000

    Unfinished section introduction with 2 grid columns

--- src/components/introduction-section/introduction-section-tablet.jsx
+++ src/components/introduction-section/introduction-section-tablet.jsx
@@ -6,23 +6,21 @@ import LittleCurve from './shared/little-curve/little-curve';
 
 const IntroductionSectionTablet = () => (
   <section className={styles.content}>
-    <div className={styles.left}>
-      <h1 className={styles.header}>MERN Stack Developer</h1>
-      <p className={styles.about}>
-        I’m full stack developer for 4 years. Currently searching for Software Developer Job. One
-        semester away from graduating as a computer security specialist. Like to play CTFs.
-      </p>
-      <div className={styles.motto}>
-        <LittleCurve />
-        <span>Performance. Readability. Tested.</span>
-      </div>
-    </div>
+    <h1 className={styles.header}>MERN Stack Developer</h1>
+    <p className={styles.about}>
+      I’m full stack developer for 4 years. Currently searching for Software Developer Job. One
+      semester away from graduating as a computer security specialist. Like to play CTFs.
+    </p>
 
-    <div className={styles.right}>
-      <Pyramid className={styles.pyramid} />
-      <Profile />
-      <span className={styles.name}>Nikita Voloshin</span>
-    </div>
+    <span className={styles.motto}>Performance. Readability. Tested.</span>
+
+    <LittleCurve />
+    <Pyramid className={styles.pyramid} />
+    <Profile />
+    <span className={styles.name}>
+      <span>Nikita&nbsp;</span>
+      <span>Voloshin</span>
+    </span>
   </section>
 );
 
--- src/components/introduction-section/shared/little-curve/little-curve.jsx
+++ src/components/introduction-section/shared/little-curve/little-curve.jsx
@@ -1,13 +1,15 @@
 import styles from './little-curve.module.scss';
 
 const LittleCurve = () => (
-  <svg className={styles['little-curve']} width="46" height="32" viewBox="0 0 46 32">
-    <path
-      d="M1 18.7164C9.45282 18.5337 6.87678 30 19.2749 30C31.673 30 32.7382 7.32257 45 2"
-      stroke="#FFBA11"
-      strokeWidth="4"
-    />
-  </svg>
+  <div className={styles.wrapper}>
+    <svg className={styles['little-curve']} viewBox="0 0 46 32">
+      <path
+        d="M1 18.7164C9.45282 18.5337 6.87678 30 19.2749 30C31.673 30 32.7382 7.32257 45 2"
+        stroke="#FFBA11"
+        // strokeWidth="4"
+      />
+    </svg>
+  </div>
 );
 
 export default LittleCurve;
