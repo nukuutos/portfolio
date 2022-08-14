0c57b8604f6361d4f5aedaa957526872f2629a46
nukuutos <nukuutos@gmail.com>
2022-08-15 00:12:37 +1000

    Add prefix to html head links

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -1,5 +1,7 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 
+const prefix = process.env.NEXT_PUBLIC_BASE_PATH || '';
+
 export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
     const initialProps = await Document.getInitialProps(ctx);
@@ -10,11 +12,11 @@ export default class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-          <link rel="manifest" href="/site.webmanifest" />
-          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+          <link rel="apple-touch-icon" sizes="180x180" href={`${prefix}/apple-touch-icon.png`} />
+          <link rel="icon" type="image/png" sizes="32x32" href={`${prefix}/favicon-32x32.png`} />
+          <link rel="icon" type="image/png" sizes="16x16" href={`${prefix}/favicon-16x16.png`} />
+          <link rel="manifest" href={`${prefix}/site.webmanifest`} />
+          <link rel="mask-icon" href={`${prefix}/safari-pinned-tab.svg`} color="#5bbad5" />
           <meta name="msapplication-TileColor" content="#da532c" />
           <meta name="theme-color" content="#ffffff" />
         </Head>
