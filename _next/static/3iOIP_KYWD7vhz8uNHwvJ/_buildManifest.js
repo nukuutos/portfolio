c4513e3f5d3f968a8d073a6683c35fb0f891bb8c
nukuutos <46343950+nukuutos@users.noreply.github.com>
2022-08-14 18:00:08 +0000

    Deploying to gh-pages from @ nukuutos/portfolio@79c34c3f4135225ac4d8abe4d8b997121fb72892 🚀

---
+++ _next/static/3iOIP_KYWD7vhz8uNHwvJ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/735-4a5fcdff048cf568.js","static/css/75dc527c2a368d98.css","static/chunks/pages/index-9b942abc5ffdcb46.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
--- _next/static/Pqn0yaRZEvQY4X7uT5n57/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/766-d34c58e6a16797d2.js","static/css/75dc527c2a368d98.css","static/chunks/pages/index-2d744cd3ad268566.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
