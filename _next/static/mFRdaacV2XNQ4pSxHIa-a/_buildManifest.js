55ca9730db192d36096157343587ec0e2ca1ede3
nukuutos <46343950+nukuutos@users.noreply.github.com>
2022-08-20 12:48:31 +0000

    Deploying to gh-pages from @ nukuutos/portfolio@f514072929a9798fc384b6685fc58b9bf6b8600e 🚀

---
+++ _next/static/mFRdaacV2XNQ4pSxHIa-a/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/735-4a5fcdff048cf568.js","static/css/d21d086667e04c2a.css","static/chunks/pages/index-606bce848e7d6fbe.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
